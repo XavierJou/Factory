eb546c232e66407059adc7098395b2634f8d1db8
Kaneze <nathaliederangere.pro@gmail.com>
2024-07-10 14:21:44 +0200

    Service Formateur et Formation

---
+++ factory-back/src/main/java/factory/service/FormationService.java
@@ -0,0 +1,57 @@
+package factory.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import factory.dao.IDAOFormation;
+import factory.model.Formation;
+
+@Service
+public class FormationService {
+
+	@Autowired
+	IDAOFormation daoFormation;
+
+	public Formation getById(Integer id) {
+		if (id == null) {
+			throw new RuntimeException("Impossible de find Formation sans id");
+		}
+		return daoFormation.findById(id).orElseThrow(RuntimeException::new);
+	}
+
+	public List<Formation> getAll() {
+		return daoFormation.findAll();
+	}
+
+	public List<Formation> getAllByTitre(String titre) {
+		return daoFormation.findAllByTitre(titre);
+	}
+
+	public Formation insert(Formation formation) {
+		if (formation.getTitre() == null) {
+			throw new RuntimeException("Impossible d'insert Formation sans titre");
+		}
+		return daoFormation.save(formation);
+	}
+
+	public Formation update(Formation formation) {
+		if (formation.getTitre() == null) {
+			throw new RuntimeException("Impossible d'insert Formation sans titre");
+		}
+		return daoFormation.save(Formation);
+	}
+
+	public void deleteById(Integer id) {
+		if (id == null) {
+			throw new RuntimeException("Impossible de delete Formation sans id");
+		}
+		daoFormation.deleteById(id);
+	}
+
+	public void delete(Formation formation) {
+		deleteById(formation.getId());
+	}
+
+}
