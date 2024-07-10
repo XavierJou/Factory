e4b936c24533923ceab539e9dbbf9e8b78bc7b1e
Kaneze <nathaliederangere.pro@gmail.com>
2024-07-10 16:44:53 +0200

    Factory rest

---
+++ factory-rest/src/main/java/factoryrest/dto/response/CustomJsonViews.java
@@ -0,0 +1,16 @@
+package factoryrest.dto.response;
+
+public class CustomJsonViews {
+
+	public static class Common {
+
+	}
+
+	public static class StagaireWithFiliere extends Common {
+
+	}
+
+	public static class FiliereWithStagiaire extends Common {
+
+	}
+}
