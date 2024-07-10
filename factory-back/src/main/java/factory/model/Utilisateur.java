c4e191fc6093d022d5acd44cebecaf4482f05c60
XavierJou <xavier.jourdaine@gmail.com>
2024-07-10 16:18:24 +0200

    JavaClass2

--- factory-back/src/main/java/factory/model/Utilisateur.java
+++ factory-back/src/main/java/factory/model/Utilisateur.java
@@ -9,7 +9,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 @Entity
 @Table(name = "utilisateur")
