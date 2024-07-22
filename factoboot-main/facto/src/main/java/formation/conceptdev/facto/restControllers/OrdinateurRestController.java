b942e328c89c336b185fe171321c46f05c67b134
clem27370 <clem27370@gmail.com>
2024-07-22 13:07:36 +0200

    Modif OrdinateurRestController

--- factoboot-main/facto/src/main/java/formation/conceptdev/facto/restControllers/OrdinateurRestController.java
+++ factoboot-main/facto/src/main/java/formation/conceptdev/facto/restControllers/OrdinateurRestController.java
@@ -6,19 +6,27 @@ import java.util.stream.Collectors;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.fasterxml.jackson.annotation.JsonView;
 
 import formation.conceptdev.facto.dto.request.OrdinateurRequest;
+import formation.conceptdev.facto.dto.response.CustomJsonViews;
 import formation.conceptdev.facto.dto.response.OrdinateurResponse;
 import formation.conceptdev.facto.entities.Ordinateur;
 import formation.conceptdev.facto.services.OrdinateurService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/ordinateur")
@@ -40,5 +48,23 @@ public class OrdinateurRestController {
 		Ordinateur o = new Ordinateur();
 		BeanUtils.copyProperties(ordinateurRequest, o);
 		return new OrdinateurResponse(ordinateurSrv.insert(o));
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void deleteById(@PathVariable("id") Integer id) {
+		ordinateurSrv.deleteById(id);
+	}
+	
+	@PostMapping("")
+	@ResponseStatus(code = HttpStatus.CREATED)
+	@JsonView(CustomJsonViews.Common.class)
+	public OrdinateurResponse create(@Valid @RequestBody OrdinateurRequest ordinateurRequest, BindingResult br) {
+		if (br.hasErrors()) {
+			throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+		}
+		Ordinateur ordinateur = new Ordinateur();
+		BeanUtils.copyProperties(ordinateurRequest, ordinateur);
+		return new OrdinateurResponse(ordinateurSrv.insert(ordinateur), false);
 	}
 }
