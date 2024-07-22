09eea703413b2d605fa5f67083be2ae077946b26
clem27370 <clem27370@gmail.com>
2024-07-22 15:20:37 +0200

    SalleRestController Fonctionnel

--- factoboot-main/facto/src/main/java/formation/conceptdev/facto/restControllers/SalleRestController.java
+++ factoboot-main/facto/src/main/java/formation/conceptdev/facto/restControllers/SalleRestController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -64,4 +65,10 @@ public class SalleRestController {
     public SalleResponse getByIdWithCours(@PathVariable Integer id) {
         return new SalleResponse(salleService.getByIdWithCours(id),false);
     }
+    
+    @DeleteMapping("/{id}")
+	@ResponseStatus(code = HttpStatus.NO_CONTENT)
+	public void deleteById(@PathVariable("id") Integer id) {
+		salleService.deleteById(id);
+	}
 }
