a17246a9360a352d679c0fecd5d1305ef6f0ce2f
eric <sortirjouer@gmail.com>
2024-07-23 14:11:00 +0200

    CORRECTION RESPONSE UTILISATERU

--- test_angular/src/app/models/utilisateur.ts
+++ test_angular/src/app/models/utilisateur.ts
@@ -1,3 +1,5 @@
+import { Formateur } from './formateur';
+import { Stagiaire } from './stagiaire';
 export class Utilisateur {
   constructor(
     public id?: number,
@@ -5,6 +7,8 @@ export class Utilisateur {
     public nom?: string,
     public prenom?: string,
     public email?: string,
-    public role?: string
+    public role?: string,
+    public formateur?: Formateur,
+    public stagiaire?: Stagiaire
   ) {}
 }
