84ec64a925a208754cf18c191a75ad252bcc04ee
Kaneze <nathaliederangere.pro@gmail.com>
2024-07-26 11:50:28 +0200

    maj animation

--- test_angular/src/app/animations.ts
+++ test_angular/src/app/animations.ts
@@ -11,35 +11,33 @@ import {
 
 export const slideInAnimation = trigger('routeAnimations', [
   transition('* <=> *', [
-    state(
-      '*',
-      style({
-        top: 0,
-        right: 0,
-        width: '100%',
-        bottom: 0,
-      })
+    style({ position: 'relative' }),
+    query(
+      ':enter, :leave',
+      [
+        style({
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+        }),
+      ],
+      { optional: true }
     ),
-
-    transition(':enter', [
-      style({
-        right: '-100%',
+    query(':enter', [style({ left: '-100%' })], { optional: true }),
+    query(':leave', animateChild(), { optional: true }),
+    group([
+      query(
+        ':leave',
+        [animate('200ms ease-out', style({ left: '100%', opacity: 0 }))],
+        {
+          optional: true,
+        }
+      ),
+      query(':enter', [animate('300ms ease-out', style({ left: '0%' }))], {
+        optional: true,
       }),
-      animate(
-        '.5s ease-in-out',
-        style({
-          right: 0,
-        })
-      ),
-    ]),
-
-    transition(':leave', [
-      animate(
-        '.5s ease-in-out',
-        style({
-          right: '-100%',
-        })
-      ),
+      query('@*', animateChild(), { optional: true }),
     ]),
   ]),
 ]);
